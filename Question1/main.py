bd4bf67530f2630847e240a548cdeea5ad885310
Josh Hamby <bubbahamby@gmail.com>
2023-12-14 15:45:20 -0700

    update

--- Question1/main.py
+++ Question1/main.py
@@ -1,21 +1,26 @@
-f = open('puzzle2.txt','r')
-lines = f.readlines()
-
-numbers = ['1','2','3','4','5','6','7','8','9','0']
-wordNumbers={'one': '1','two': '2','three': '3','four': '4','five': '5','six': '6','seven': '7','eight': '8','nine': '9'}
+def convertLine(line):
+    wordNumbers={'nine': '9','eight': '8','seven': '7','six': '6','five': '5','four': '4','three': '3','two': '2','one': '1'}
+    string = ''
+    for i in line:
+        for word, number in wordNumbers.items():
+                if (word in string):
+                    line = line.replace(word, number)
+        string += i
+    return line
+with open('puzzle.txt', 'r') as f:
+    lines = f.readlines()
+numbers = ['1','2','3','4','5','6','7','8','9']
 sum = 0
 for line in lines:
     first = ''
     last = ''
-    for word in wordNumbers.keys():
-        if word in line:
-            line = line.replace(word,wordNumbers[word])
+    line = convertLine(line)
     for i in range(len(line)):
         if first == '':
             if line[i] in numbers:
                 first = line[i]
         if line[i] in numbers:
             last = line[i]
-    # print('Input: ',line, " First: ", first, " Last: ", last)
+    print('Input: ',line, " First: ", first, " Last: ", last)
     sum += int(first + last)
     print(sum)
